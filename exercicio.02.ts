f86f433984cc18bea2057736ba53a17f9f68c24e
wirlleym <wirlley17@gmail.com>
2022-06-28 00:41:08 -0300

    excercicio-sem01

---
+++ exercicio.02.ts
@@ -0,0 +1,42 @@
+// Exercício.02
+
+interface User {
+    name: string;
+    age: number;
+    occupation?: string;
+}
+
+interface Admin extends User{
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+persons.forEach(logPerson);
---
+++ exercicio.03.ts
@@ -0,0 +1,48 @@
+// Exercício.03
+
+interface User extends Admin {
+    name: string;
+    age: number;
+    occupation?: string;
+}
+
+interface Admin {
+    role?: string;
+}
+
+export type Person = User;
+
+export const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+export function logPerson(person: Person) {
+    let additionalInformation: string | undefined;
+    if (person.role) {
+        additionalInformation = person.role;
+    } else {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
